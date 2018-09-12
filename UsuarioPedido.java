09df144a0427b2d4eb94a71bc968156905fd53dd
Oscar Salvador Rojas <salvadorrojas219@gmail.com>
2018-09-11 18:22:19 -0600

    Update UsuarioPedido.java

--- UsuarioPedido.java
+++ UsuarioPedido.java
@@ -11,22 +11,60 @@ package progra;
  */
 
 public class UsuarioPedido {
-public int pCedula;
-public String PNombre;
-public String pDireccion;
-public String pCorreo;
-public int pTelefono;
+	public int pCedula;
+	public String PNombre;
+	public String pDireccion;
+	public String pCorreo;
+	public int pTelefono;
 
-public UsuarioPedido(int pCedula,String pNombre, String pDireccion, String pCorreo,int pTelefono){
-    this.pTelefono= pTelefono;
-    this.pCedula=pCedula;
-    this.PNombre=pNombre;
-    this.pDireccion=pDireccion;
-    this.pCorreo=pCorreo;
-           
+
+	public UsuarioPedido(int pCedula,String pNombre, String pDireccion, String pCorreo,int pTelefono){
+	    this.pTelefono= pTelefono;
+	    this.pCedula=pCedula;
+	    this.PNombre=pNombre;
+	    this.pDireccion=pDireccion;
+	    this.pCorreo=pCorreo;
+       
     
-}
-public int getCedula(){
-    return pCedula;
-}
-}
+	}
+	
+	public class Cola{
+		UsuarioPedido Pedido;
+		Cola siguiente;
+		
+		public Cola() {
+			this.Pedido=null;
+			this.siguiente=null;
+		}
+		public Cola (UsuarioPedido Pedido) {
+			this.Pedido=Pedido;
+			this.siguiente=null;
+		}
+	}
+	
+	public Cola head;
+	public Cola tail;
+	
+	public int getpCedula() {
+		return pCedula;
+	}
+
+	public String getPNombre() {
+		return PNombre;
+	}
+	
+	
+	public String getpDireccion() {
+		return pDireccion;
+	}
+	
+	
+	public String getpCorreo() {
+		return pCorreo;
+	}
+	
+	
+	public int getpTelefono() {
+		return pTelefono;
+	}
+	
